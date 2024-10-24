bfb7c4cb56af92602529bffb9468444afd3c6de5
Eclipse <44953687+iToxic9@users.noreply.github.com>
2024-10-24 07:10:19 +0100

    Added new Class for Grid 2
    small bug fix for goal location (forgot to subtract 1 from x and y), and added manual grid 2 setup. Everything works!

--- JavaSearch-2023/src/uk/ac/hw/macs/search/AStarGrid2.java
+++ JavaSearch-2023/src/uk/ac/hw/macs/search/AStarGrid2.java
@@ -1,6 +1,6 @@
 package uk.ac.hw.macs.search;
 
-public class AStar {
+public class AStarGrid2 {
 
 	public static void main(String[] args) {
 		
@@ -10,10 +10,10 @@ public class AStar {
 		
 		//Grid 1 ================ This is where you can setup different grid arrangements ================
 		// ================ You can change these VV ================
-		//IMPORTANT: Change these values to set the goal node (x,y)=(5,2).
+		//IMPORTANT: Change these values to set the goal node (x,y)=(3,4).
 		//Keep in mind it starts at 0
-		final int endX = 5;
-		final int endY = 2;
+		final int endX = 3;
+		final int endY = 4;
 		// ================ Don't change these VV ================
 
 		int heuristic = Math.abs(endX) + Math.abs(endY); //Gives heuristic value for starting node
@@ -27,96 +27,101 @@ public class AStar {
 		// (x,y)
 		// (0,0) Start
 		// root Node
-		// (0,1) E
+		// (0,1) D
 		heuristic = getHeuristic(0,1, endX, endY);
-		Node E = new Node(new AStarStates(heuristic, false));
-		root.addChild(E, grayValue);
-		// (0,2) L
+		Node D = new Node(new AStarStates(heuristic, false));
+		root.addChild(D, grayValue);
+		// (0,2) J
 		heuristic = getHeuristic(0,2, endX, endY);
-		Node L = new Node(new AStarStates(heuristic, false));
-		root.addChild(L, whiteValue);
-		// (0,3) P
+		Node J = new Node(new AStarStates(heuristic, false));
+		D.addChild(J, whiteValue);
+		// (0,3) N
 		heuristic = getHeuristic(0,3, endX, endY);
-		Node P = new Node(new AStarStates(heuristic, false));
-		L.addChild(P, grayValue);
+		Node N = new Node(new AStarStates(heuristic, false));
+		J.addChild(N, grayValue);
+		// (0,4) T
+		heuristic = getHeuristic(0,4, endX, endY);
+		Node T = new Node(new AStarStates(heuristic, false));
+		N.addChild(T, grayValue);
 		// (1,0) A
 		heuristic = getHeuristic(1,0, endX, endY);
 		Node A = new Node(new AStarStates(heuristic, false));
 		root.addChild(A, whiteValue);
-		// (1,1) F
+		// (1,1) E
 		heuristic = getHeuristic(1,1, endX, endY);
-		Node F = new Node(new AStarStates(heuristic, false));
-		A.addChild(F, grayValue);
-		E.addChild(F, grayValue);
-		// (1,2) M
+		Node E = new Node(new AStarStates(heuristic, false));
+		A.addChild(E, whiteValue);
+		D.addChild(E, whiteValue);
+		// (1,2) K
 		heuristic = getHeuristic(1,2, endX, endY);
-		Node M = new Node(new AStarStates(heuristic, false));
-		F.addChild(M, grayValue);
-		L.addChild(M, grayValue);
-		// (1,3) null
-
+		Node K = new Node(new AStarStates(heuristic, false));
+		E.addChild(K, whiteValue);
+		J.addChild(K, whiteValue);
+		// (1,3) O
+		heuristic = getHeuristic(1,3, endX, endY);
+		Node O = new Node(new AStarStates(heuristic, false));
+		K.addChild(O, whiteValue);
+		N.addChild(O, whiteValue);
+		// (1,4) null
+		
 		// (2,0) null
 
-		// (2,1) H
+		// (2,1) F
 		heuristic = getHeuristic(2,1, endX, endY);
-		Node H = new Node(new AStarStates(heuristic, false));
-		F.addChild(H, whiteValue);
+		Node F = new Node(new AStarStates(heuristic, false));
+		E.addChild(F, whiteValue);
 		// (2,2) null
-
-		// (2,3) null
-
+		
+		// (2,3) P
+		heuristic = getHeuristic(2,3, endX, endY);
+		Node P = new Node(new AStarStates(heuristic, false));
+		O.addChild(P, grayValue);
+		// (2,4) null
+		
 		// (3,0) B
 		heuristic = getHeuristic(3,0, endX, endY);
 		Node B = new Node(new AStarStates(heuristic, false));
-		// (3,1) I
+		// (3,1) H
 		heuristic = getHeuristic(3,1, endX, endY);
-		Node I = new Node(new AStarStates(heuristic, false));
-		H.addChild(I, grayValue);
-		B.addChild(I, grayValue);
-		// (3,2) N
+		Node H = new Node(new AStarStates(heuristic, false));
+		F.addChild(H, grayValue);
+		B.addChild(H, grayValue);
+		// (3,2) L
 		heuristic = getHeuristic(3,2, endX, endY);
-		Node N = new Node(new AStarStates(heuristic, false));
-		I.addChild(N, grayValue);
+		Node L = new Node(new AStarStates(heuristic, false));
+		H.addChild(L, whiteValue);
 		// (3,3) Q
 		heuristic = getHeuristic(3,3, endX, endY);
 		Node Q = new Node(new AStarStates(heuristic, false));
-		N.addChild(F, whiteValue);
+		L.addChild(Q, whiteValue);
+		P.addChild(Q, whiteValue);
+		// (3,4) Goal!
+		Q.addChild(G, whiteValue);
 		// (4,0) C
 		heuristic = getHeuristic(4,0, endX, endY);
 		Node C = new Node(new AStarStates(heuristic, false));
 		B.addChild(C, whiteValue);
-		// (4,1) J
+		// (4,1) I
 		heuristic = getHeuristic(4,1, endX, endY);
-		Node J = new Node(new AStarStates(heuristic, false));
-		C.addChild(J, whiteValue);
-		I.addChild(J, whiteValue);
-		// (4,2) O
+		Node I = new Node(new AStarStates(heuristic, false));
+		H.addChild(I, whiteValue);
+		C.addChild(I, whiteValue);
+		// (4,2) M
 		heuristic = getHeuristic(4,2, endX, endY);
-		Node O = new Node(new AStarStates(heuristic, false));
-		J.addChild(O, grayValue);
-		N.addChild(O, grayValue);
+		Node M = new Node(new AStarStates(heuristic, false));
+		I.addChild(M, whiteValue);
+		L.addChild(M, whiteValue);
 		// (4,3) R
 		heuristic = getHeuristic(4,3, endX, endY);
 		Node R = new Node(new AStarStates(heuristic, false));
-		O.addChild(R, grayValue);
+		M.addChild(R, grayValue);
 		Q.addChild(R, grayValue);
-		// (5,0) D
-		heuristic = getHeuristic(5,0, endX, endY);
-		Node D = new Node(new AStarStates(heuristic, false));
-		C.addChild(D, whiteValue);
-		// (5,1) K
-		heuristic = getHeuristic(5,1, endX, endY);
-		Node K = new Node(new AStarStates(heuristic, false));
-		D.addChild(K, whiteValue);
-		J.addChild(K, whiteValue);
-		// (5,2) Goal!
-		K.addChild(G, whiteValue);
-		O.addChild(G, whiteValue);
-		// (5,3) T
-		heuristic = getHeuristic(5,3, endX, endY);
-		Node T = new Node(new AStarStates(heuristic, false));
-		G.addChild(T, grayValue);
-		R.addChild(T, grayValue);
+		// (4,4) U
+		heuristic = getHeuristic(4,4, endX, endY);
+		Node U = new Node(new AStarStates(heuristic, false));
+		R.addChild(U, grayValue);
+		G.addChild(U, grayValue);
+
 
 		// ================ Run A* Search ================
 
