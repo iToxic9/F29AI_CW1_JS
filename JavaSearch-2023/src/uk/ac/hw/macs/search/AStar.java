210d38ec7aaa0172d6f62b7ddee1183fbabb4458
Eclipse <44953687+iToxic9@users.noreply.github.com>
2024-10-24 03:02:41 +0100

    Working Grid Initialization
    Neat solution to making a grid of white and gray cells. Missing heuristic value assignment...

--- JavaSearch-2023/src/uk/ac/hw/macs/search/AStar.java
+++ JavaSearch-2023/src/uk/ac/hw/macs/search/AStar.java
@@ -1,27 +1,32 @@
 package uk.ac.hw.macs.search;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AStar {
-
-	//Node S = new Node(null);
-	
-	private Grid[][] grid;
-
-	
-	
-
-	public AStar(int width, int height, int startX, int startY, int goalX, int goalY) {
-        grid = new Grid[width][height];
-    }
-
-	
 	
 	public static void main(String[] args) {
 
 		//Node OUT = new Node());
 		
 		//Grid 1
-		
-		
+		Node S = new Node(new AStarStates(0, false));
+		Node G = new Node(new AStarStates(1, true));
+
+		final String[] whiteCells = {"a","c","d","h","j","k","l","m","q"};
+		final String[] grayCells = {"b","e","f","i","n","o","p","r","t"};
+		Map<String, Node>  frontier = new TreeMap<String, Node>();
+
+		//Create Grid
+		//20 Nodes including Start and Goal
+		//White cells
+		for (int i = 0; i < whiteCells.length; i++) {
+			frontier.put(whiteCells[i], new Node(new AStarStates(1))); //Creates white cells
+		}
+		//Gray cells
+		for (int i = 0; i < grayCells.length; i++) {
+			frontier.put(grayCells[i], new Node(new AStarStates(2))); //Creates gray cells
+		}
 		
 		SearchOrder order = new AStarSearchOrder();
 		SearchProblem problem = new SearchProblem(order);
--- JavaSearch-2023/src/uk/ac/hw/macs/search/AStarStates.java
+++ JavaSearch-2023/src/uk/ac/hw/macs/search/AStarStates.java
@@ -2,38 +2,34 @@ package uk.ac.hw.macs.search;
 
 public class AStarStates implements State {
 
-    public Grid[][] grid;
-
-    private int startX, startY;
-
-	private int goalX, goalY;
-    
-    //Run in main to assign H value to nodes
-    public int getHeuristic() {
-            //Manhatten Distance formula, sum of absolute values of 
-            //the difference between the goal's coordinates and the current nodes coordinates
-            int h = 0; //temp
-            for(int i = 0; i < grid.length; i++){
-                for(int j = 0; j < grid[i].length; j++){
-                    grid[i][j] = new Grid(i, j);
-                    grid[i][j].heuristicCost = Math.abs(i - goalX) + Math.abs(j - goalY);
-                    grid[i][j].solution = false;
-                }
-            }
-
-            return h;
-    }
-
-
-    //check if current node solution is true
-    //TODO Fix this return to work
-	public boolean isGoal(int i, int j) {
-		return grid[i][j].solution;
+    private int value;
+	private boolean goal;
+	
+	public AStarStates (int value) {
+		this(value, false);
 	}
-    
-    @Override
-    public boolean isGoal() {
-        return false;
-    }
+	
+	public AStarStates (int value, boolean goal) {
+		this.value = value;
+		this.goal = goal;
+	}
+
+	@Override
+	public boolean isGoal() {
+        return this.goal;
+	}
+
+	@Override
+	public int getHeuristic() {
+        int h;
+        //h = Math.abs(G.value - this.value))+Math.abs(G.value - this.value));
+        
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "IntegerState [value=" + value + ", goal=" + goal + "]";
+	}
 
 }
